de2e4ac153c09f4de389a27d70a9e842c2776a5f
duspic <duspic77@gmail.com>
2023-04-20 20:12:06 +0200

    add basic cog files

---
+++ predict.py
@@ -0,0 +1,11 @@
+# Prediction interface for Cog ⚙️
+# https://github.com/replicate/cog/resolve/main/docs/python.md
+
+from cog import BasePredictor, Input, Path
+
+class Predictor(BasePredictor):
+    def setup(self):
+        pass
+
+    def predict(self):
+        pass
